da2177792c58796723b7caa70581814bddf2e5f1
Neota Moe <neota.moe@gmail.com>
2017-05-12 14:47:26 -0500

    add button working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,4 +2,21 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AssignmentController', function() {
   console.log('NG');
+
+  var vm = this;
+
+  vm.addAssignment = function(){
+    console.log('add button clicked');
+
+    var objectToSend = {
+      assignment: vm.assignmentIn,
+      name: vm.nameIn,
+      score: vm.scoreIn,
+      date_completed: vm.date_completedIn
+    }; // end objectToSend
+    console.log('this is objectToSend', objectToSend);
+    
+  }; // end getAssignment
+
+
 }); // end controller
